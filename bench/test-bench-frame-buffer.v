fc68dec647cbaca3e505abfd09070ca47c01d3e2
Gabriel Teixeira <gabrieldiegoteixeira@gmail.com>
2012-12-14 17:06:07 -0200

    Partially implemented copy of the frame buffer from the input file to the internal frame buffer

--- bench/test-bench-frame-buffer.v
+++ bench/test-bench-frame-buffer.v
@@ -4,21 +4,38 @@ module test_bench (
   // Outputs
   clk, data
 );
+  parameter FRAME_WIDTH  = 352;
+  parameter FRAME_HEIGHT = 288;
+  parameter FRAME_STRIDE = FRAME_WIDTH;
+  parameter FRAME_SIZE   = FRAME_WIDTH*FRAME_HEIGHT;
+
   output clk;
-  output [7:0] data;
+  output [63:0] data;
 
-  reg    clk;
-  reg [7:0] data;
+  reg         clk;
+  reg         reset;
+  reg [63:0]  data;
 
-  integer   fd;
-  integer   code;
-  reg [8*10:1] str;
+  integer     fd;
+  integer     code;
+  integer     i;
 
-  reg [11:0] stride;
-  reg [11:0] width;
-  reg [11:0] height;
+  reg [11:0]  stride;
+  reg [11:0]  width;
+  reg [11:0]  height;
 
-  reg setup_frame;
+  reg         setup_frame;
+
+  reg [10:0]  x,y;
+  reg [31:0]  file_pos;
+
+  reg [FRAME_SIZE*8-1:0] frame_buffer;
+
+  wire        mem_addr;
+  wire        mem_data;
+
+  reg         fb_write;
+  reg  [63:0] fb_data;
 
   initial begin
 
@@ -28,15 +45,26 @@ module test_bench (
     fd = $fopen("bus_cif.yuv","r"); 
     clk = 0;
     data = 0;
+    reset = 1;
     code = 1;
+    setup_frame = 0;
 
-//    $monitor("data = %x", data);
+    @(posedge clk);
+    reset = 0;
+
+    /* Setup the parameters of the frame buffer */
+    stride = FRAME_WIDTH;
+    width  = FRAME_WIDTH;
+    height = FRAME_HEIGHT;
+    file_pos = 0;
 
     @(posedge clk);
     setup_frame = 1;
-    stride = 2048;
-    width = 1920;
-    height = 1080;
+
+    /* Possible bug in iverilog? */
+    @(posedge clk);
+    @(negedge clk);
+    setup_frame = 0;
 
     @(posedge clk);
     setup_frame = 0;
@@ -44,19 +72,39 @@ module test_bench (
     width = 0;
     height = 0;
 
-    while (code) begin
-      code = $fread(data, fd);
+    code = $fread(frame_buffer, fd);
+
+    if(!code) begin
+      $write("Error reading file.");
+      $finish;
+    end
+
+    file_pos = 0;
+
+    fb_write = 1;
+    @(posedge clk);
+
+    /* Loop over the input pixels */
+    while (file_pos != FRAME_SIZE) begin
+      for(i=0; i<8*8; i=i+1) begin
+        fb_data[i] = frame_buffer[file_pos+i];
+      end
+
       @(posedge clk);
+
+      file_pos = file_pos+8*8;
+      
     end
 
     $finish;
 
   end // initial begin
 
-  always #5 clk = ~clk;
+  always #1 clk = ~clk;
 
   frame_buffer #(.MEM_WIDTH(64)) test_frame_buffer (
-    .clk(clk), .setup_frame(setup_frame),
+    .clk(clk), .reset(reset), .x(x), .y(y),
+    .fb_write(fb_write), .fb_data(fb_data), .setup_frame(setup_frame),
     .stride_in(stride), .width_in(width), .height_in(height)
   );
 
--- rtl/frame-buffer.v
+++ rtl/frame-buffer.v
@@ -2,14 +2,20 @@ module frame_buffer (
   clk,reset,
 
   // Interface with memory
-  out_addr,
-  in_data,
+  mem_addr,
+  mem_data,
+  mem_data_out,
+  mem_read,
+  mem_write,
 
   // Pixel loader
   x,y,
   read_block,
   blk_line,
   blk_line_rdy,
+
+  fb_write,
+  fb_data,
 
   // Frame setup
   stride_in,
@@ -19,20 +25,26 @@ module frame_buffer (
 
 );
   parameter MEM_WIDTH=64;
+  parameter BLK_WIDTH=8;
 
   input clk,reset;
 
-  // TODO: What is the bit width of the memory access?
-  //  A: Don't bother with that now and use 64 as a stopgap.
-  output [20:0] out_addr; // Output address to the memory block
-  input  [MEM_WIDTH-1:0] in_data;  // Input data from the memory block
+  output [20:0] mem_addr; // Output address to the memory block
+  input  [MEM_WIDTH-1:0] mem_data; // Input data from the memory block
+  output [MEM_WIDTH-1:0] mem_data_out; // Output data to the memory block
+  output mem_read;      // Signal to read from the memory
+  output mem_write;     // Signal to write to the memory
 
   input  [10:0] x,y;    // Block x and y position to be read
   input  read_block;    // Pulse to send block position and start reading
                         // block data
-  output [127:0] blk_line;      // Load an entire line at each cycle
+  output [BLK_WIDTH*8-1:0] blk_line; // Load an entire line at each cycle
   output blk_line_rdy;  // Pixel line ready to be read
 
+  input  fb_write;      // Pulse to send a entire frame buffer
+  input  [BLK_WIDTH*8-1:0] fb_data; // Data to the frame buffer
+
+  // Input of the measures of the frame
   input  [11:0] stride_in;
   input  [11:0] width_in;
   input  [11:0] height_in;
@@ -42,11 +54,35 @@ module frame_buffer (
   reg [11:0] width;
   reg [11:0] height;
 
+  reg [20:0] mem_addr;
+  reg [MEM_WIDTH-1:0] mem_data_out;
+  reg mem_write;
+
+  reg [10:0] x_pos,y_pos;
+
   always @ (posedge clk) begin
     if(setup_frame == 1) begin
       stride = stride_in;
       width = width_in;
       height = height_in;
+    end
+    else if(fb_write == 1) begin
+      x_pos = 0;
+      y_pos = 0;
+
+      /* Receive the first line of pixels just in the next cycle */
+      while(y_pos != height) begin
+        mem_addr = (x_pos + y_pos * stride) * 8;
+        mem_data_out = fb_data;
+        mem_write = "1";
+        if(x_pos != width) begin
+          x_pos = x_pos + BLK_WIDTH;
+        end else begin
+          x_pos = 0;
+          y_pos = y_pos + 1;
+        end
+        @(posedge clk);
+      end
     end
   end
 
